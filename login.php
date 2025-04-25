623a0e40f93302e699b0b7484987f32ae64ea660
Diran Torosyan <122069583+Diran-Torosyan@users.noreply.github.com>
2025-04-25 12:47:40 -0700

    Update login.php

--- login.php
+++ login.php
@@ -9,6 +9,8 @@ if (session_status() === PHP_SESSION_NONE) {
 
 require __DIR__ . '/config.php';
 
+require 'config.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Sanitize inputs
     $email    = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
