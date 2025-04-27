239346595d6c18038b098a4f50e9f76a44dc2aac
Diran Torosyan <122069583+Diran-Torosyan@users.noreply.github.com>
2025-04-27 10:21:19 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // adjust host, dbname, user, pass to match config.php
-    $pdo = new PDO('mysql:host=localhost;dbname=424Project;charset=utf8', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=424Project;charset=utf8', 'appuser', '424project');
     echo "✅ PDO connected successfully!";
 } catch (PDOException $e) {
     die("❌ PDO connection failed: " . $e->getMessage());
